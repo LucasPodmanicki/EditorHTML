6c99da28cb046eab8eaa3205e51b63c5e4349044
LucasPodmanicki <Podmanicki.fer@Outlook.com>
2022-05-12 11:56:25 -0300

    Adicionar arquivos de projeto.

---
+++ EditorHTML/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EditorHTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Menu.Show();
+
+        }
+    }
+}
